bac05d2e3f1b98f840997bd7dc3d283e931018cd
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-08-06 14:55:29 +0200

    Update gps.js

--- js/gps.js
+++ js/gps.js
@@ -178,7 +178,7 @@ function processTtsQueue() {
         const utterance = new SpeechSynthesisUtterance(cleanedText);
         const voice = getBestDutchVoice();
         if (voice) utterance.voice = voice;
-        //utterance.lang = "nl-NL";
+        utterance.lang = "nl-NL";
         utterance.rate = 0.95;
 
         utterance.onend = () => {
